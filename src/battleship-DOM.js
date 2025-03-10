6e1fe2fd871e1305291a0e0ed26594d7d20a2cbc
shibe7202 <46942349+shibe7202@users.noreply.github.com>
2025-03-10 16:17:35 -0700

    UI improvements

--- src/battleship-DOM.js
+++ src/battleship-DOM.js
@@ -1,9 +1,12 @@
-function createGrid(grid) {
+import sailBoat from './sail-boat.svg'
+import hex from './checkbox-blank-circle-outline.svg'
+
+function createGrid(grid, player) {
     grid.innerHTML = ''
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
             const tile = document.createElement('div')
-            tile.classList.add('tile')
+            tile.classList.add('tile', player)
             tile.setAttribute('id', i.toString() + j.toString())
             grid.appendChild(tile)
         }
@@ -16,19 +19,32 @@ function renderGameboard(player1, player2) {
     player1Tiles.forEach((tile) => {
         const coord = tile.getAttribute('id')
         const ship = player1.gameboard.board[coord[0]][coord[1]]
+        tile.innerHTML = ''
         if (ship.occupied.hits > 0 && tile.classList.contains('clicked')) {
             tile.innerHTML = 'Hit!'
         } else if (ship.occupied === 'missedShot') {
-            tile.innerHTML = '-'
+            const hexImage = new Image()
+            hexImage.src = hex
+            hexImage.classList.add('hex')
+            tile.appendChild(hexImage)
+        } else if (typeof ship.occupied === 'object') {
+            const sailBoatImage = new Image()
+            sailBoatImage.src = sailBoat
+            sailBoatImage.classList.add('sailBoat')
+            tile.appendChild(sailBoatImage)
         }
     })
     player2Tiles.forEach((tile) => {
         const coord = tile.getAttribute('id')
         const ship = player2.gameboard.board[coord[0]][coord[1]]
+        tile.innerHTML = ''
         if (ship.occupied.hits > 0 && tile.classList.contains('clicked')) {
             tile.innerHTML = 'Hit!'
         } else if (ship.occupied === 'missedShot') {
-            tile.innerHTML = '-'
+            const hexImage = new Image()
+            hexImage.src = hex
+            hexImage.classList.add('hex')
+            tile.appendChild(hexImage)
         }
     })
 }
--- src/index.js
+++ src/index.js
@@ -1,46 +1,24 @@
 import './style.css'
-import { Player } from './battleship-app'
+import { Gameboard, Player } from './battleship-app'
 import { createGrid, renderGameboard, declareWinner } from './battleship-DOM'
 
-const player1 = new Player()
-const player2 = new Player()
+let player1 = new Player()
+let player2 = new Player()
 let currentTarget = player2
 let twoPlayerMode = false
-
-/*
-player1.gameboard.place([
-    [1, 1],
-    [1, 2],
-])
-player1.gameboard.place([
-    [3, 4],
-    [4, 4],
-    [5, 4],
-])
-
-player2.gameboard.place([
-    [5, 5],
-    [6, 5],
-    [7, 5],
-])
-
-player2.gameboard.place([
-    [3, 7],
-    [3, 8],
-])
-    */
-
-player2.gameboard.place([[1, 1]])
 
 const firstBoard = document.querySelector('.left-board')
 const secondBoard = document.querySelector('.right-board')
 
 function startGame() {
-    createGrid(firstBoard)
-    createGrid(secondBoard)
+    createGrid(firstBoard, 'player1')
+    createGrid(secondBoard, 'player2')
     const allTiles = document.querySelectorAll('.tile')
     allTiles.forEach((tile) => {
         tile.addEventListener('click', () => {
+            if (tile.classList.contains('player1')) {
+                return
+            }
             const coord = tile.getAttribute('id')
             tile.classList.add('clicked')
             currentTarget.gameboard.receiveAttack(coord)
@@ -56,10 +34,78 @@ function startGame() {
     })
 }
 
+const placeButton = document.querySelector('.place-button')
+placeButton.addEventListener('click', () => {
+    createPlacements(player1)
+    createPlacements(player2)
+    renderGameboard(player1, player2)
+})
+
+function createPlacements(player) {
+    player.gameboard = new Gameboard()
+    for (let i = 0; i < 2; i++) {
+        placeShipHorizontally(player)
+    }
+    for (let i = 0; i < 2; i++) {
+        placeShipVertically(player)
+    }
+}
+
+function placeShipHorizontally(player) {
+    let coord1 = [getRandomInt(), getRandomInt()]
+    let coord2
+    let coord3
+    if (coord1[1] + 1 < 10) {
+        coord2 = [coord1[0], coord1[1] + 1]
+        if (coord2[1] + 1 < 10) {
+            coord3 = [coord1[0], coord2[1] + 1]
+        } else {
+            coord3 = [coord1[0], coord1[1] - 1]
+        }
+    } else {
+        coord2 = [coord1[0], coord1[1] - 1]
+        coord3 = [coord1[0], coord2[1] - 1]
+    }
+    if (
+        player.gameboard.board[coord1[0]][coord1[1]].occupied ||
+        player.gameboard.board[coord2[0]][coord2[1]].occupied ||
+        player.gameboard.board[coord3[0]][coord3[1]].occupied
+    ) {
+        placeShipHorizontally(player)
+    } else {
+        player.gameboard.place([coord1, coord2, coord3])
+    }
+}
+
+function placeShipVertically(player) {
+    let coord1 = [getRandomInt(), getRandomInt()]
+    let coord2
+    let coord3
+    if (coord1[0] + 1 < 10) {
+        coord2 = [coord1[0] + 1, coord1[1]]
+        if (coord2[0] + 1 < 10) {
+            coord3 = [coord2[0] + 1, coord1[1]]
+        } else {
+            coord3 = [coord1[0] - 1, coord1[1]]
+        }
+    } else {
+        coord2 = [coord1[0] - 1, coord1[1]]
+        coord3 = [coord2[0] - 1, coord1[1]]
+    }
+    if (
+        player.gameboard.board[coord1[0]][coord1[1]].occupied ||
+        player.gameboard.board[coord2[0]][coord2[1]].occupied ||
+        player.gameboard.board[coord3[0]][coord3[1]].occupied
+    ) {
+        placeShipVertically(player)
+    } else {
+        player.gameboard.place([coord1, coord2, coord3])
+    }
+}
+
 function computerTurn() {
     const randomRow = getRandomInt()
     const randomCol = getRandomInt()
-    console.log([randomRow, randomCol])
     if (
         player1.gameboard.board[randomRow][randomCol].occupied != 'missedShot'
     ) {
